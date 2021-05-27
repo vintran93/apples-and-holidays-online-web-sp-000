f3bc53bdd708a445d7b05b2cd9b3bbbd2f894bd9
Vincent Tran <vincentuong@yahoo.com>
2021-05-27 02:42:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   holiday_supplies[:winter][:christmas] = "tree"
-  holiday_supplies[:winter][:new_years] = "champaigne"
+  holiday_supplies[:winter][:new_years] = "champagne"
 end
 
 
