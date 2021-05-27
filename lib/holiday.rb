fdd84380fab667ce1b50a2f1a2e1de6b8fac6651
Vincent Tran <vincentuong@yahoo.com>
2021-05-27 03:00:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
